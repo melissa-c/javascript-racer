f62b96e8b73d74ca73f496f4cdfc46cbd12fe4ba
mel <dyslixec@hotmail.com>
2016-01-20 22:42:40 +1300

    if applied this commit will add more pseudocode

--- js/jQuery.js
+++ js/jQuery.js
@@ -10,39 +10,50 @@ $(document).on("keyup", someFunction)
 
 
 /*
-Start state = the only active table cells are the first 
-table cells in each row.
+Start state = the only active table cells are the first table 
+cells in each row.
 */
 
 /*
-Prompt = instructions stating the assigned keys to press for 
-each player.
+Prompt = 
+instructions stating the assigned keys to press for each player:
+"Welcome to the Presidential Race for the White House. 
+Press C to move Clinton.
+Press T to move Trump.
+Good luck, 'Merica!"
 */
 
 /*
-Assign the keys = player1 is C (keycode 67), 
-player2 is T (keycode 84)
+Assign the keys = 
+player1_row id with table cell class active will be C (keycode 67), 
+player2_row id with table cell class active will be T (keycode 84)
 */
 
 /* 
-When an assigned key is pressed the corresponding table cell
-in that player's row changes from an active cell to a normal
-table cell and the next table cell to the right becomes an 
-active cell.
+When an assigned key is pressed =
+the table cell in the corresponding player's row changes from an 
+active cell to a normal table cell and the next table cell to the 
+right becomes an active cell.
 */
 
 /*
-When the last table cell in one of the rows becomes an active 
+When the last table cell in one of the player's rows becomes an active 
 cell the game ends and the key presses are disabled.
 */
 
 /*
-Alert = the player whose row had its last table cell become an 
-active cell is announced the winner. The alert also states that
-they can play again. 
+The player whose row had its last table cell become an active cell 
+is the winner. 
 */
 
 /*
-The table reverts back to the start state.
+Alert = 
+If player1 wins: "Clinton won! Celebrate with the interns!"
+If player2 wins: "Trump won! Find a bunker - the end is nigh!" 
+Each alert also states that they can play again. 
+*/
+
+/*
+The table reverts back to its start state.
 */
 
