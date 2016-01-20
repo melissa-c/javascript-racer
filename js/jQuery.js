f51b7de8f56cc9c8842f8ac78effd66db1513eb2
mel <dyslixec@hotmail.com>
2016-01-20 22:10:52 +1300

    if applied this commit will add pseudocode to js file and typo correx to html and css

---
+++ js/jQuery.js
@@ -0,0 +1,48 @@
+// wait until everything on page has loaded
+$("document").ready(function() {
+  //run the code
+});
+
+
+function updatePlayerPosition("player1");
+
+$(document).on("keyup", someFunction)
+
+
+/*
+Start state = the only active table cells are the first 
+table cells in each row.
+*/
+
+/*
+Prompt = instructions stating the assigned keys to press for 
+each player.
+*/
+
+/*
+Assign the keys = player1 is C (keycode 67), 
+player2 is T (keycode 84)
+*/
+
+/* 
+When an assigned key is pressed the corresponding table cell
+in that player's row changes from an active cell to a normal
+table cell and the next table cell to the right becomes an 
+active cell.
+*/
+
+/*
+When the last table cell in one of the rows becomes an active 
+cell the game ends and the key presses are disabled.
+*/
+
+/*
+Alert = the player whose row had its last table cell become an 
+active cell is announced the winner. The alert also states that
+they can play again. 
+*/
+
+/*
+The table reverts back to the start state.
+*/
+
